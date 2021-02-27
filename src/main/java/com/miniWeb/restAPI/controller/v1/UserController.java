7c73526acf87e93196770348847e1c387b5cc518
DavidKim <zkffhtm6523@naver.com>
2021-02-28 03:40:39 +0900

    Swagger 연동 완료

--- src/main/java/com/miniWeb/restAPI/controller/v1/UserController.java
+++ src/main/java/com/miniWeb/restAPI/controller/v1/UserController.java
@@ -2,30 +2,35 @@ package com.miniWeb.restAPI.controller.v1;
 
 import com.miniWeb.restAPI.entity.User;
 import com.miniWeb.restAPI.repo.UserJpaRepo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1, user"})
 @RequiredArgsConstructor
 @RestController // 결과값을 JSON으로 출력합니다.
 @RequestMapping(value = "/v1")
 public class UserController {
+
     private final UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다.")
     @GetMapping(value = "/user")
     public List<User> findAllUser() {
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 등록", notes = "회원을 등록한다.")
     @PostMapping(value = "/user")
-    public User save() {
+    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원 이름", required = true) @RequestParam String name) {
         User user = User.builder()
-                .uid("yumi@naver.com")
-                .name("유미")
+                .uid(uid)
+                .name(name)
                 .build();
         return userJpaRepo.save(user);
     }
